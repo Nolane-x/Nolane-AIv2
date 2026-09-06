b1e2e18fb00c2a01e8aeb2aa515a65e7ad221c0d
Nolane-x <casioreview20@gmail.com>
2026-09-06 19:53:33 +0700

    test: keep ceremony import gate in model lane

--- tests/test_exp282_ceremony_import.py
+++ tests/test_exp282_ceremony_import.py
@@ -1,4 +1,4 @@
-def test_exp282_confirmatory_ceremony_module_exists():
+def test_model_exp282_confirmatory_ceremony_module_exists():
     from nolane_ai.experiments import exp282_confirmatory_ceremony
 
     assert exp282_confirmatory_ceremony is not None
