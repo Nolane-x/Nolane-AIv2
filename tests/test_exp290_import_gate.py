982dc2e41d797324065cb0525f153c564fcae681
Nolane-x <casioreview20@gmail.com>
2026-09-13 10:31:34 +0700

    test(exp290): add correspondence RED import gate

--- tests/test_exp290_import_gate.py
+++ tests/test_exp290_import_gate.py
@@ -9,3 +9,7 @@ def test_exp290_geometry_module_exists() -> None:
 
 def test_exp290_world_module_exists() -> None:
     assert importlib.util.find_spec("nolane_ai.experiments.exp290_transfer_worlds") is not None
+
+
+def test_exp290_correspondence_module_exists() -> None:
+    assert importlib.util.find_spec("nolane_ai.experiments.exp290_correspondence") is not None
